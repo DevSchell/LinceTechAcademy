2143f75f449be8a3c893fe62094ba0b1afb85086
DevSchell <schelldev@gmail.com>
2025-03-29 13:27:32 -0300

    Atividade prática 4 - Coleções

--- formacao_dart/ap2_colecoes/ap4.dart
+++ formacao_dart/ap2_colecoes/ap4.dart
@@ -1,9 +1,12 @@
 void main() {
-
   //Criando os valores de cada chave do mapa
   List<String> cidadesMG = ["Belo Horizonte", "Berlinda", "Juiz de Fora"];
   List<String> cidadesPR = ["Cascavel", "Curitiba", "Foz do Iguaçu"];
-  List<String> cidadesSC = ["Gaspar", "Florianópolis", "Blumenau"]; // Fora de ordem alfabética, pois vou testar o método Sort() com strings...
+  List<String> cidadesSC = [
+    "Gaspar",
+    "Florianópolis",
+    "Blumenau",
+  ]; // Fora de ordem alfabética, pois vou testar o método Sort() com strings...
   List<String> cidadesSP = ["Campinas", "Guaruhos", "São Paulo"];
 
   //Criando o mapa organizado já por chave e ordem alfabética
@@ -11,11 +14,21 @@ void main() {
     "MG": cidadesMG,
     "PR": cidadesPR,
     "SC": cidadesSC,
-    "SP": cidadesSP
+    "SP": cidadesSP,
   };
 
   // Requisito 1: Imprimir siglas dos estados
   print("Estados: ${mapa.keys.toString()}");
 
   // Requisito 2: Imprimir todas as cidades da chave SC em ordem alfabética
+  mapa["SC"]?.sort();
+  print(mapa["SC"]);
+
+  // Requisito 3: Imprimir todas as cidades em ordem alfabética
+  List<String> chaves = mapa.keys.toList();
+  chaves.sort();
+  for (var i = 0; i < chaves.length; i++) {
+    print("Estado ${chaves[i]}");
+    print("Cidades: ${mapa[chaves[i]]}");
+  }
 }
