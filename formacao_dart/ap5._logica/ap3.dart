b37e93a3207036341d3e6d0cfdfe47b3ee96af8d
DevSchell <schelldev@gmail.com>
2025-04-02 16:28:34 -0300

    atividade prática 4 - elementos lógicos + hotfix: adição de comentários em ap3.dart

--- formacao_dart/ap5._logica/ap3.dart
+++ formacao_dart/ap5._logica/ap3.dart
@@ -1,17 +1,19 @@
 import 'dart:math';
 
 void main() {
+  //Simulando valor do usuário
   int valor = Random().nextInt(901) + 100;
 
   print("A soma dos números pares entre 0 e $valor é ${somarPares(valor)}");
 }
 
+//Função requisitada
 int somarPares(int limitador) {
   int resultado = 0;
 
   for (var i = 0; i <= limitador; i++) {
     if (i % 2 == 0) {
-      resultado += i;
+      resultado += i; //resultado vai se incrementando sempre que condição é true
     }
   }
 
